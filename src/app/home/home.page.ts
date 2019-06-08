d2451d2b3be0b885b81c0ccda10763878dae96ff
Maryna Avramenko <maavra@pascalium.com>
2019-06-08 20:29:06 +0300

    create project, added structure and page: home, add-company

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: 'home.page.pug',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
   constructor() {}
+
 
 }
