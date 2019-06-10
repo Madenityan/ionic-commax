9bab87a0af9df0fc7276ded1dd216e56dacc4b22
Maryna Avramenko <maavra@pascalium.com>
2019-06-10 23:29:09 +0300

    added infinite scroll in home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CompaniesService} from '../services/companies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.pug',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  listCompanies = [];
+  numberPerPage = 5;
+  currentPage = 0;
 
+  constructor(private companiesService: CompaniesService) {}
 
+  ngOnInit() {
+    // this.listCompanies = this.companiesService.getSavedCompany();
+    this.loadNextPage();
+    // this.loadNextPage();
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.loadNextPage();
+      event.target.complete();
+    }, 700);
+  }
+
+  loadNextPage() {
+    const allData = this.companiesService.getSavedCompany();
+    const offset = this.currentPage * this.numberPerPage;
+    this.listCompanies = this.listCompanies.concat(allData.slice(offset, offset + this.numberPerPage));
+    this.currentPage++;
+  }
 }
