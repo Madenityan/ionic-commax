3db6bb2b46cf7eb7064af1967e9855fab5b18958
Maryna Avramenko <maavra@pascalium.com>
2019-06-10 17:46:06 +0300

    added active sheets

--- src/app/add-company/add-company.page.ts
+++ src/app/add-company/add-company.page.ts
@@ -1,6 +1,9 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import {ActionSheetController} from '@ionic/angular';
+import {ImagePicker} from '@ionic-native/image-picker/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-add-company',
@@ -12,7 +15,12 @@ export class AddCompanyPage implements OnInit {
     public addCompanyForm: FormGroup;
     photo = '';
 
-  constructor(private formBuilder: FormBuilder, private camera: Camera) { }
+    constructor(private formBuilder: FormBuilder,
+                private camera: Camera,
+                private imagePicker: ImagePicker,
+                public actionSheetController: ActionSheetController,
+                private storage: Storage) {
+    }
 
   ngOnInit() {
       this.addCompanyForm = this.formBuilder.group({
@@ -23,24 +31,59 @@ export class AddCompanyPage implements OnInit {
       });
   }
 
-  addCompany() {
-    console.log(this.addCompanyForm.value);
-  }
+    async openActionSheet() {
+        const actionSheet = await this.actionSheetController.create({
+            header: 'Add photo',
+            buttons: [{
+                text: 'Gallery',
+                role: 'destructive',
+                icon: 'images',
+                handler: () => {
+                    this.addImage();
+                }
+            }, {
+                text: 'Camera',
+                icon: 'camera',
+                handler: () => {
+                    this.addPhoto();
+                }
+            }]
+        });
+        await actionSheet.present();
+    }
 
-    addImage() {
+    addCompany() {
+        console.log(this.addCompanyForm.value);
+        this.storage.set('company', this.photo);
+    }
+
+    addPhoto() {
         const options: CameraOptions = {
-            quality: 100,
+            quality: 25,
             destinationType: this.camera.DestinationType.DATA_URL,
             encodingType: this.camera.EncodingType.JPEG,
             mediaType: this.camera.MediaType.PICTURE
         };
         this.camera.getPicture(options).then((imageData) => {
-                this.photo = 'data:image/jpeg;base64' + imageData;
+                this.photo = 'data:image/jpeg;base64,' + imageData;
                 this.addCompanyForm.controls.picture.setValue(this.photo);
             },
-            (err) => {});
-
+            (err) => {
+                console.error(err);
+            });
     }
 
-
+    addImage() {
+        const options = {
+            maximumImagesCount: 1,
+            minWidth: 200,
+            quality: 25,
+            outputType: 1
+        };
+        this.imagePicker.getPictures(options).then((results) => {
+            this.photo = 'data:image/jpeg;base64,' + results;
+        }, (err) => {
+                console.error(err);
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Camera} from '@ionic-native/camera/ngx';
+import {ImagePicker} from '@ionic-native/image-picker/ngx';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
-      AppComponent,
+      AppComponent
   ],
   entryComponents: [],
   imports: [
       BrowserModule,
       IonicModule.forRoot(),
+      IonicStorageModule.forRoot(),
       AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Camera,
+    ImagePicker,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
