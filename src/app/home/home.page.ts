4946dfaec538afdfea8f4cbdfe337c7e5823b775
Maryna Avramenko <maavra@pascalium.com>
2019-06-11 14:21:15 +0300

    added remove button and removal functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CompaniesService} from '../services/companies.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,22 @@ export class HomePage implements OnInit {
   listCompanies = [];
   numberPerPage = 5;
   currentPage = 0;
+  allCompanies;
 
-  constructor(private companiesService: CompaniesService) {}
+  constructor(private companiesService: CompaniesService, private storage: Storage) {
+    this.allCompanies = this.companiesService.getSavedCompany();
+  }
 
   ngOnInit() {
+    this.addId();
     this.loadNextPage();
+  }
+
+  addId() {
+    const i = 0;
+    this.allCompanies.forEach((item, index) => {
+      item.id = index + 1;
+    });
   }
 
   loadData(event) {
@@ -31,5 +43,17 @@ export class HomePage implements OnInit {
     const offset = this.currentPage * this.numberPerPage;
     this.listCompanies = this.listCompanies.concat(allData.slice(offset, offset + this.numberPerPage));
     this.currentPage++;
+  }
+
+  removeCompany(id) {
+    const index = this.allCompanies.findIndex(company => company.id === id);
+    if (index > -1) {
+      this.allCompanies.splice(index, 1);
+    }
+    this.storage.set('сompanies', this.allCompanies).then(res => {
+      this.listCompanies = [];
+      this.currentPage = 0;
+      this.loadNextPage();
+    });
   }
 }
