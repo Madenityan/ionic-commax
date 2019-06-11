14c760e055c50077ef3473d97378e21bcc24d698
Maryna Avramenko <maavra@pascalium.com>
2019-06-12 00:32:50 +0300

    added component details

---
+++ src/app/details-company/details-company.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DetailsCompanyPage } from './details-company.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DetailsCompanyPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [DetailsCompanyPage]
+})
+export class DetailsCompanyPageModule {}
---
+++ src/app/details-company/details-company.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-company',
+  templateUrl: './details-company.page.html',
+  styleUrls: ['./details-company.page.scss'],
+})
+export class DetailsCompanyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
